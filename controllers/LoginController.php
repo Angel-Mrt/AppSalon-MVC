812cd2ed1cc35a9b7e9460cb8a927ce54ddff457
AngelMrt <angelmrtnic@gmail.com>
2024-03-20 14:33:45 -0600

    Creando Controladores

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,16 +1,18 @@
 <?php
+
 namespace Controllers;
 
 use MVC\Router;
 
-class LoginController {
+class LoginController
+{
     public static function login(Router $router)
     {
         $router->render('auth/login');
     }
     public static function logout()
     {
-        echo 'Desde  Logout';
+        echo 'En  Logout';
     }
     public static function olvide(Router $router)
     {
@@ -18,11 +20,10 @@ class LoginController {
     }
     public static function recuperar()
     {
-        echo 'Desde  recuperar';
+        echo 'En  recuperar';
     }
     public static function crear(Router $router)
     {
         $router->render('auth/crear-cuenta', []);
     }
 }
-
--- public/index.php
+++ public/index.php
@@ -12,8 +12,5 @@ $router->get('/', [LoginController::class, 'login']);
 $router->post('/', [LoginController::class, 'login']);
 $router->get('/logout', [LoginController::class, 'logout']);
 
-
-
-
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
