fdb3d7f4dd5147884641baf957a2e723adde8717
AngelMrt <angelmrtnic@gmail.com>
2024-03-22 18:39:33 -0600

    Creando los controladores para el Login

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Usuario;
 use MVC\Router;
 
 class LoginController
@@ -24,6 +25,26 @@ class LoginController
     }
     public static function crear(Router $router)
     {
-        $router->render('auth/crear-cuenta', []);
+        $usuario = new Usuario;
+
+        //Alertas Vacias
+        $alertas = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $usuario->sincronizar($_POST);
+            $alertas = $usuario->validarNuevaCuenta();
+
+            // Revisar que alerta este vacio 
+            if (empty($alertas)) {
+                // Verificar que el usuario no este registrado
+                echo 'Pasaste la verificacion';
+            }
+        }
+
+        $router->render('auth/crear-cuenta', [
+            'usuario' => $usuario,
+            'alertas' => $alertas
+        ]);
     }
 }
