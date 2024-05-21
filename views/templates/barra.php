bda3af0772596f04e5578a2bfc5470405855b4d2
AngelMrt <angelmrtnic@gmail.com>
2024-05-21 13:59:04 -0600

    Creando un menu para el administrador, asi como las vistas para los servicios

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -2,3 +2,10 @@
     <p>Hola: <span> <?php echo $nombre ?? ''; ?> </span></p>
     <a class="boton" href="/logout">Cerrar Sesión</a>
 </div>
+<?php if (isset($_SESSION['admin'])) { ?>
+    <div class="barra-servicios">
+        <a class="boton" href="/admin">Ver Citas</a>
+        <a class="boton" href="/servicios">Ver Servicios</a>
+        <a class="boton" href="/servicios/crear">Nuevo Servicio</a>
+    </div>
+<?php } ?>
